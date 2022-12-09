ac8538884e649dabdbb761470e79422d6d4e872b
Mynewgi <mina.m.monsef.apr@gmail.com>
2022-12-09 09:08:32 -0500

    first commit

---
+++ src/test/java/automation_code_assignment_7/T_Mobile.java
@@ -0,0 +1,19 @@
+package automation_code_assignment_7;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class T_Mobile {
+
+	public static void main(String[] args) {
+		WebDriver driver;
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.t-mobile.com/");
+
+	}
+
+}
